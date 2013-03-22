a36dfea1e1d3c58e5f316d3b84cc120bb11ce617
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-23 01:39:30 +0900

    Catch up modification of target directories on starting watch

--- app/watch.rb
+++ app/watch.rb
@@ -12,42 +12,71 @@ class Watch
     @directories.each do |dir|
       $stderr.puts "  * #{dir}"
     end
-    Listen.to *@directories, :filter => EPUB_RE do |modified, added, removed|
-      modified.each do |file_path|
-        next unless file_path =~ EPUB_RE
-        file_path.force_encoding 'UTF-8'
-        begin
-          Remove.new(@db_path, file_path).run
-          Add.new(@db_path, file_path).run
-          notify %Q|MODIFIED: #{file_path}|
-        rescue => error
-          $stderr.puts error
+    catch_up
+    begin
+      Listen.to *@directories, :filter => EPUB_RE do |modified, added, removed|
+        modified.each do |file_path|
+          next unless file_path =~ EPUB_RE
+          file_path.force_encoding 'UTF-8'
+          begin
+            Remove.new(@db_path, file_path).run
+            Add.new(@db_path, file_path).run
+            notify %Q|MODIFIED: #{file_path}|
+          rescue => error
+            $stderr.puts error
+          end
+        end
+        added.each do |file_path|
+          next unless file_path =~ EPUB_RE
+          file_path.force_encoding 'UTF-8'
+          begin
+            Add.new(@db_path, file_path).run
+            notify %Q|ADDED: #{file_path}|
+          rescue => error
+            $stderr.puts error
+          end
+        end
+        removed.each do |file_path|
+          next unless file_path =~ EPUB_RE
+          file_path.force_encoding 'UTF-8'
+          begin
+            Remove.new(@db_path, file_path).run
+            notify %Q|REMOVED: #{file_path}|
+          rescue => error
+            $stderr.puts error
+          end
         end
       end
-      added.each do |file_path|
-        next unless file_path =~ EPUB_RE
-        file_path.force_encoding 'UTF-8'
+    ensure
+      FileUtils.touch exit_time_file
+    end
+  end
+
+  private
+
+  def exit_time
+    @exittime ||= File.mtime(exit_time_file)
+  end
+
+  def exit_time_file
+    File.expand_path('../../exittime', @db_path)
+  end
+
+  def catch_up
+    @directories.each do |dir|
+      Dir["#{dir}/**/*.epub"].each do |file_path|
+        next if File.file? exit_time_file and File.mtime(file_path) < exit_time
         begin
+          removed = Remove.new(@db_path, file_path).run rescue nil
           Add.new(@db_path, file_path).run
-          notify %Q|ADDED: #{file_path}|
-        rescue => error
-          $stderr.puts error
-        end
-      end
-      removed.each do |file_path|
-        next unless file_path =~ EPUB_RE
-        file_path.force_encoding 'UTF-8'
-        begin
-          Remove.new(@db_path, file_path).run
-          notify %Q|REMOVED: #{file_path}|
+          operation = removed ? 'MODIFIED' : 'ADDED'
+          notify "#{operation}: #{file_path}"
         rescue => error
           $stderr.puts error
         end
       end
     end
   end
-
-  private
 
   def notify(message)
     $stderr.puts message
