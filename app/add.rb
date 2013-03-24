84aec98e3b00c50d8f7de935c3406702fe893361
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-24 23:56:53 +0900

    Move logic of adding to lib

--- app/add.rb
+++ app/add.rb
@@ -1,22 +1,11 @@
 class Add
-  def initialize(db_path, file_path)
-    raise "File not readable: #{file_path}" unless File.readable? file_path
-    @db_path, @file_path = db_path, Pathname(file_path)
+  def initialize(db_dir, file_path)
+    @file_path = Pathname.new(file_path)
+    raise "File not readable: #{@file_path}" unless @file_path.readable?
+    @db = EPUB::Search::Database.new(db_dir)
   end
 
   def run
-    location = @file_path.expand_path
-    book = EPUB::Parser.parse(location)
-    Groonga::Database.open @db_path do
-      pages = Groonga['Pages']
-
-      book.each_content do |content|
-        next unless content.media_type == 'application/xhtml+xml'
-        pages.add('location' => location.to_s,
-                  'iri'      => content.href.to_s,
-                  'title'    => book.title,
-                  'content'  => Nokogiri.XML(Nokogiri.XML(content.read).search('body').first.to_xml).content)
-      end
-    end
+    @db.add @file_path
   end
 end
--- lib/epub/search/database.rb
+++ lib/epub/search/database.rb
@@ -34,6 +34,23 @@ module EPUB
           table.index 'Pages.content'
         end
       end
+
+      def add(file_path)
+        file_path = Pathname.new(file_path) unless file_path.kind_of? Pathname
+        location = file_path.expand_path
+        book = EPUB::Parser.parse(location)
+        Groonga::Database.open db_file.to_path do
+          pages = Groonga['Pages']
+
+          book.each_content do |content|
+            next unless content.media_type == 'application/xhtml+xml'
+            pages.add('location' => location.to_s,
+                      'iri'      => content.href.to_s,
+                      'title'    => book.title,
+                      'content'  => Nokogiri.XML(Nokogiri.XML(content.read).search('body').first.to_xml).content)
+          end
+        end
+      end
     end
   end
 end
