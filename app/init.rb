514a819682edf4a7bf1297360fc3bc18025ab82d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-25 05:55:18 +0900

    Remove exittime file only when initing with --force

--- app/init.rb
+++ app/init.rb
@@ -6,7 +6,9 @@ class Init
   def run(force=false)
     $stderr.puts "create database #{@db.db_file}"
     @db.init force
-    exit_time_file = @db.db_dir.join('../exittime')
-    exit_time_file.delete if exit_time_file.exist?
+    if force
+      exit_time_file = @db.db_dir.join('../exittime')
+      exit_time_file.delete if exit_time_file.exist?
+    end
   end
 end
