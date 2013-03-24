84d161fb0a28f862832b5813457032302440a9bb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-25 00:44:48 +0900

    Modify slightly

--- app/search.rb
+++ app/search.rb
@@ -7,9 +7,7 @@ class Search
   def run
     re = /#{Regexp.escape(@word)}/o
     hl = HighLine.new if $stdout.tty?
-    books = Hash.new {|h, iri|
-      h[iri] = EPUB::Parser.parse(iri)
-    }
+    books = Hash.new {|h, iri| h[iri] = EPUB::Parser.parse(iri)}
     @db.search @word do |result|
       result.each_pair do |location, records|
         puts "#{records.first.title}(#{location})"
