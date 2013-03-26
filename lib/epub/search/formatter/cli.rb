b747313985e58406a743ea933be638068a65da6b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-26 13:52:16 +0900

    Ignore case in search result

--- lib/epub/search/formatter/cli.rb
+++ lib/epub/search/formatter/cli.rb
@@ -7,14 +7,14 @@ module EPUB
         end
 
         def format
-          word_re = /#{Regexp.escape(@word)}/o
+          word_re = /(?<word>#{Regexp.escape(@word)})/io
           highlighter = HighLine.Style(:red, :bold) if highlight?
           @data.each_pair do |location, records|
             records.each do |record|
               record.content.each_line do |line|
                 next unless line =~ word_re
                 result = line.chomp
-                result.gsub! word_re, highlighter.color(@word) if highlight?
+                result.gsub! word_re, highlighter.color($~[:word]) if highlight?
                 result << "  [#{record.page_title}(#{record.book_title}): #{location} - #{record.iri}]"
                 puts result
               end
