df46a15efe8ee1e1ef9338fad024ad695f22a419
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-29 03:47:19 +0900

    Gray book titles in search result

--- lib/epub/search/formatter/cli.rb
+++ lib/epub/search/formatter/cli.rb
@@ -9,13 +9,16 @@ module EPUB
         def format
           word_re = /(?<word>#{Regexp.escape(@word)})/io
           highlighter = HighLine.Style(:red, :bold) if highlight?
+          gray = HighLine.Style(:gray) if highlight?
           @data.each_pair do |location, records|
             records.each do |record|
               record.content.each_line do |line|
                 next unless line =~ word_re
                 result = line.chomp
                 result.gsub! word_re, highlighter.color($~[:word]) if highlight?
-                result << "  [#{record.page_title}(#{record.book_title}): #{location} - #{record.iri}]"
+                book_info = "[#{record.page_title}(#{record.book_title}): #{location} - #{record.iri}]"
+                book_info = gray.color(book_info)
+                result << book_info
                 puts result
               end
             end
