32a2b3ffc7a3c47f719c52ac6c0966f8a932b8cb
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-31 01:52:12 +0900

    Bind 127.0.0.1 for server

--- app/server.rb
+++ app/server.rb
@@ -4,6 +4,6 @@ class Server
   end
 
   def run
-    Rack::Server.start :app => EPUB::Search::Server.new(@db)
+    Rack::Server.start :app => EPUB::Search::Server.new(@db), :Host => '127.0.0.1'
   end
 end
